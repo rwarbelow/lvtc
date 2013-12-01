6c5c953eaeee5f7b9a98c542bcbe07af83ffc9cc
Rachel Warbelow <rachelwarbelow@gmail.com>
2013-12-01 01:41:56 -0800

    testing production config for email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,6 +34,16 @@ Lvtc::Application.configure do
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
+
+  # Specify what domain to use for mailer URLs
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache

09ebc066c1c45540f920e48ec6868620137e5386
Rachel Warbelow <rachelwarbelow@gmail.com>
2013-12-01 21:22:57 -0800

    fixed javascript for stripe

--- app/assets/javascripts/race_registrations.js
+++ app/assets/javascripts/race_registrations.js
@@ -1,20 +1,22 @@
+var stripeResponseHandler = function(status, response) {
+  var $form = $('#payment-form');
+  console.log(response)
+  if (response.error) {
+    $('#stripe-error').text(response.error.message);
+    $form.find('button').prop('disabled', false);
+  } else {
+    var token = response.id;
+    $form.append($('<input type="hidden" name="stripeToken" />').val(token));
+    $form.get(0).submit();
+  }
+};
+
 $(document).ready(function(){
-  Stripe.setPublishableKey(stripePubKey);
-  var stripeResponseHandler = function(status, response) {
-	  var $form = $('#payment-form');
-	  if (response.error) {
-	    $form.find('.payment-errors').text(response.error.message);
-	    $form.find('button').prop('disabled', false);
-	  } else {
-	    var token = response.id;
-	    $form.append($('<input type="hidden" name="stripeToken" />').val(token));
-	    $form.get(0).submit();
-	  }
-	};
+  Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));
   $("#payment-form").submit(function(){
     var form = $(this);
     form.find('button').prop('disabled', true);
-    Stripe.card.createToken(form, stripeResponseHandler);
+    Stripe.createToken(form, stripeResponseHandler);
     return false;
   })
 })
